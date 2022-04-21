c10012ae16b9a2c073fe56c565850f5a32f512b2
Kurtis Fleming <kurtallica@hotmail.com>
2022-04-21 17:02:05 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <InlineEditor />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import InlineEditor from "./components/InlineEditor.vue";
+
+export default defineComponent({
+  name: "App",
+
+  components: {
+    InlineEditor,
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+});
+</script>
---
+++ src/components/InlineEditor.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <div id="main" align="center" justify="center" v-cloak @click="hideTooltip">
+      <div class="tooltip" @click.stop v-if="show_tooltip">
+        <v-text-field
+          label="Filled"
+          filled
+          v-model="text_content"
+        ></v-text-field>
+      </div>
+      <p @click.stop="toggleTooltip">✎ {{ text_content }}</p>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InlineEditor",
+
+  data() {
+    return {
+      show_tooltip: false,
+      text_content: "Edit Me.",
+    };
+  },
+  methods: {
+    hideTooltip: function () {
+      this.show_tooltip = false;
+    },
+    toggleTooltip: function () {
+      this.show_tooltip = !this.show_tooltip;
+    },
+  },
+});
+</script>
+
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
